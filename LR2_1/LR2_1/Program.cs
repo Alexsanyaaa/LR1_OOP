71d4106da9b0c966f26b79cd7f91c6e70992171b
Alexsanyaaa <golovinaleksandr1702@mail.ru>
2025-11-11 15:35:51 +0500

    Add files via upload

---
+++ LR2_1/LR2_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace css
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+            double apo, boo, css, ew, ddd;
+            double f, g;
+
+            apo = Convert.ToDouble(Console.ReadLine());
+            boo = Convert.ToDouble(Console.ReadLine());
+            css = Convert.ToDouble(Console.ReadLine());
+            ew = Convert.ToDouble(Console.ReadLine());
+            ddd = Convert.ToDouble(Console.ReadLine());
+
+            if ((apo == 0) || (boo == 0) || (ew < 0) || ddd == 0 || css == 0)
+            {
+                Console.WriteLine("ERROR");
+            }
+            else
+            {
+                f = 1 / 100  - 1 / apo - 1 / (boo * boo);
+                Console.WriteLine(String.Format("{0:0.000}", f));
+
+                g = 1 / (css * css) + System.Math.Sqrt(ew) / (System.Math.Pow(ddd, 3));
+                Console.WriteLine(String.Format("{0:0.000}", g));
+            }
+
+            Console.SetOut(save_out);
+            Console.SetIn(save_in);
+            new_out.Close();
+            new_in.Close();
+        }
+    }
+}
